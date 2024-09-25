5f99641294d95fe4385f5ee25e9314566d91062f
CaughtdlV <lebedev.next2005@gmail.com>
2024-09-25 21:06:06 +0300

    25/09

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, True, 'String')
+print('Immutable tuple:', immutable_var)
+mutable_list = [1, True, 'String']
+mutable_list[0] = 'New String'
+mutable_list[1] = 10
+mutable_list[2] = False
+print('Mutable list:',immutable_var, mutable_list)
