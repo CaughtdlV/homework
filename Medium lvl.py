48a3bb185a7c3fa982eec5782e108f84428a59ba
CaughtdlV <lebedev.next2005@gmail.com>
2024-11-02 05:28:05 +0300

    25/09

---
+++ Medium lvl.py
@@ -0,0 +1,18 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+student1 = (sum(grades[0])/len(grades[0]))
+student2 = (sum(grades[1])/len(grades[1]))
+student3 = (sum(grades[2])/len(grades[2]))
+student4 = (sum(grades[3])/len(grades[3]))
+student5 = (sum(grades[4])/len(grades[4]))
+grades = [student1, student2, student3, student4, student5]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = (list(students))
+students.sort()
+dict_student_grades = {}
+len(grades)
+dict_student_grades[students[0]] = grades[0]
+dict_student_grades[students[1]] = grades[1]
+dict_student_grades[students[2]] = grades[2]
+dict_student_grades[students[3]] = grades[3]
+dict_student_grades[students[4]] = grades[4]
+print(dict_student_grades)
