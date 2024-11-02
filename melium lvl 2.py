1f547bdbda6b244fe8dad72326182d2a9e9e1775
CaughtdlV <lebedev.next2005@gmail.com>
2024-11-02 08:53:33 +0300

    25/09

---
+++ melium lvl 2.py
@@ -0,0 +1,17 @@
+import random
+password = (4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+password2 = ()
+number1 = random.choice(password)
+number2 = random.choice(password)
+while True:
+    for i in range(len(password)):
+        print(i)
+
+
+    # if (password
+    #
+    #
+    #
+    #     // sum(number1, number2)) == 0:
+    #     print()
+
